efac5e9a94cbc8deb73da91605bb516c23c940a2
cdeskilson <46496874+cdeskilson@users.noreply.github.com>
2021-03-27 19:01:59 -0500

    Remove quotes

--- professional-website/src/components/Work/config.js
+++ professional-website/src/components/Work/config.js
@@ -1,6 +1,6 @@
 export const reviewsConfig = [
    {
-    title: "''Review of <i>Sho</i> By Douglas Kearney''",
+    title: "Review of <i>Sho</i> By Douglas Kearney",
     journal: "The Arkansas International",
     link: "https://www.arkint.org/reviews/sho-capsule-review",
    }
@@ -8,101 +8,101 @@ export const reviewsConfig = [
 
 export const publishedConfig = [
    {
-    title: "''XNA-->XNA''; ''Ode to an Anti-Joke''",
+    title: "XNA-->XNA; Ode to an Anti-Joke",
     journal: "The Shore",
     link: "https://www.theshorepoetry.org/issue-9",
    },
    {
-    title: "''Self-Portrait with Werewolf”; “The Shape of Water''",
+    title: "Self-Portrait with Werewolf”; “The Shape of Water",
     journal: "Anti-Heroin Chic",
     link: "http://heroinchic.weebly.com/blog/poetry-by-cd-eskilson",
   },
   
     {
-    title: "''Dystychipaphobia, or Fear of Accidents''",
+    title: "Dystychipaphobia, or Fear of Accidents",
     journal: "Kissing Dynamite Poetry",
     link: "https://www.kissingdynamitepoetry.com/cd-eskilson-fear-of-accidents.html",
   },
   
     {
-    title: "''Siblinghood”; “Discombobulated''",
+    title: "Siblinghood”; “Discombobulated",
     journal: "Homology Lit",
     link: "https://homologylit.com/2020/07/31/cd-eskilson/",
   },
   
   {
-    title: "''Portrait with Inconclusive Lab Results''",
+    title: "Portrait with Inconclusive Lab Results",
     journal: "Barren Magazine",
     link: "https://barrenmagazine.com/portrait-with-inconclusive-lab-results/",
   },
   { 
-    title: "''Burning Haibun After Geryon''", 
+    title: "Burning Haibun After Geryon", 
     journal: "Redivider",
     link: "http://www.redividerjournal.com/2020/05/cd-eskilson-burning-haibun-after-geryon/",
   },
   {
-    title: "''Becoming Poltergeist''",
+    title: "Becoming Poltergeist",
     journal: "Ghost City Review",
     link: "https://ghostcitypress.com/poetry-420/2020/4/20/cd-eskilson",
   },
   {
-    title: "''Obscura''",
+    title: "Obscura",
     journal: "The Cortland Review",
     link: "https://www.cortlandreview.com/issue/84/eskilson.php",
   },
   {
-    title: "''After Failing to Come Out Again, the Apparition Arrives''",
+    title: "After Failing to Come Out Again, the Apparition Arrives",
     journal: "Vagabond City Lit",
     link:
       "https://vagabondcitylit.com/2020/02/10/after-failing-to-come-out-again-the-apparition-comes-by-cd-eskilson/",
   },
   {
-    title: "''Recipe for Roasted Broccoli;'' ''Mouth Sores''",
+    title: "Recipe for Roasted Broccoli; Mouth Sores",
     journal: "Moonchild Magazine",
     link: "https://www.moonchildmag.net/cdeskilson.html",
   },
   {
-    title: "''Bowie''",
+    title: "Bowie",
     journal: "Defunkt Magazine (Volume 2)",
     link: "https://www.defunktmag.com/",
   },
   {
-    title: "''King Ghidorah''",
+    title: "King Ghidorah",
     journal: "Peach Mag",
     link: "https://www.peachmgzn.com/cd-eskilson",
   },
   {
     title:
-      "''Self-Portrait with Underbite;'' ''Creation Story for a Minor Goddess of Dysphoria''",
+      "Self-Portrait with Underbite; Creation Story for a Minor Goddess of Dysphoria",
     journal: "What Are Birds? Journal",
     link:
       "https://static1.squarespace.com/static/5b7c554196e76fcbb0bf14df/t/5d7dc1b87e85cf79bafe210a/1568522680421/CD+Eskilson.pdf",
   },
   {
-    title: "''Gorgon''",
+    title: "Gorgon",
     journal: "Yes Poetry",
     link: "http://www.yespoetry.com/news/cd-eskilson-i-was-outnumbered",
   },
   {
     title:
-      "''Reboot of Aliens to Cast Myself;'' ''What Starts a List Poem About OCD''",
+      "Reboot of Aliens to Cast Myself; What Starts a List Poem About OCD",
     journal: "Butter",
     link: "https://www.bttrprss.com/cd-eskilson",
   },
   {
-    title: "''Remix of ‘White Ferrari’''",
+    title: "Remix of ‘White Ferrari’",
     journal: "The Cardiff Review",
     link:
       "https://www.cardiffreview.com/single-post/2019/02/13/Remix-of-White-Ferrari",
   },
   {
-    title: "''I Grew Up''",
+    title: "I Grew Up",
     journal: "Teen Vogue",
     link:
       "https://www.teenvogue.com/story/teen-guide-for-voting-in-the-midterms",
   },
   {
-    title: "''Lafe''",
+    title: "Lafe",
     journal: "After The Pause",
     link: "https://afterthepause.com/2016/09/01/fall-2016/",
   }
